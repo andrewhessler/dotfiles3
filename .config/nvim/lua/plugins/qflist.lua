7c7857379547695b3a64206099643a13c5cdaaff
Andrew Hessler <adhessler@gmail.com>
2026-03-31 21:44:27 -0500

    sync

--- .config/nvim/lua/plugins/qflist.lua
+++
@@ -1,3 +0,0 @@
-return {
-    "itchyny/vim-qfedit", config = function() end
-}
--- .config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter').install({ "c", "rust", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
-      "javascript", "typescript", "tsx", "zig", "wgsl" })
-  end
-}
