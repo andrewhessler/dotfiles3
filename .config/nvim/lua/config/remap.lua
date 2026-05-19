0e2812d8f16ee86ba46e39f1986d48b3e8d43bc1
Andrew Hessler <andrew.hessler@mastery.net>
2026-05-19 16:28:59 -0500

    sync

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -72,6 +72,15 @@ vim.keymap.set('n', '<leader>gb', function()
   push_and_switch(sha)
 end, { desc = 'Git switch to blame commit for current line' })
 
+vim.keymap.set('n', '<leader>gp', function()
+  local parent = vim.fn.system('git rev-parse HEAD~1'):gsub('%s+', '')
+  if vim.v.shell_error ~= 0 then
+    vim.notify('No parent commit', vim.log.levels.WARN)
+    return
+  end
+  push_and_switch(parent)
+end, { desc = 'Git switch to parent of current commit' })
+
 vim.keymap.set('n', '<leader>gu', function()
   if #blame_stack == 0 then
     vim.notify('Blame stack is empty', vim.log.levels.WARN)
@@ -87,11 +96,14 @@ vim.keymap.set('n', '<leader>gu', function()
   vim.cmd('edit');
 end, { desc = 'Undo last blame switch' })
 
-vim.keymap.set('n', '<leader>gp', function()
-  local parent = vim.fn.system('git rev-parse HEAD~1'):gsub('%s+', '')
-  if vim.v.shell_error ~= 0 then
-    vim.notify('No parent commit', vim.log.levels.WARN)
+vim.keymap.set('n', '<leader>gg', function()
+  if #blame_stack == 0 then
+    vim.notify('Blame stack is empty', vim.log.levels.WARN)
     return
   end
-  push_and_switch(parent)
-end, { desc = 'Git switch to parent of current commit' })
+
+  local base = blame_stack[1]
+  vim.fn.system('git switch ' .. base)
+
+  vim.cmd('edit');
+end, { desc = 'Undo last blame switch' })
